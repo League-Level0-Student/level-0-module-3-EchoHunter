3096611e9c9501af8a0929eca77ab99c84709c30
League Student <league@iMac-18.attlocal.net>
2019-08-01 18:25:34 -0700

    :gun:
    crazy cat lady, High low, R U happy

---
+++ src/_04_are_you_happy/R_u_Happy.java
@@ -0,0 +1,14 @@
+package _04_are_you_happy;
+
+import javax.swing.JOptionPane;
+
+public class R_u_Happy {
+public static void main(String[] args) {
+	String answer;
+	answer = JOptionPane.showInputDialog( "Are You Happy?");
+	if(answer .equals("yes")) {
+		JOptionPane.showMessageDialog(null, "Keep on doing what you're doing");
+	}
+	else if (answer .equals("no"));
+}
+}
